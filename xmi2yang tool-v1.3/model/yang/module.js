f2f39115d519b26638ebfda7e1fcfcdf0314a70a
Hui Ding <valen_ding@foxmail.com>
2016-03-03 22:10:35 +0800

    fix bugs
    Fix the namespace and revision bugs.

--- xmi2yang tool-v1.3/model/yang/module.js
+++ xmi2yang tool-v1.3/model/yang/module.js
@@ -46,8 +46,24 @@ Module.prototype.writeNode = function (layer) {
     var contact;
     this.contact == "" || this.contact == undefined ? contact = "" : contact = PRE + "\tcontact " + this.contact + ";\r\n";
     var revis;
-    var date=new Date();
-    revis=date.toLocaleDateString();
+    //var date=new Date();
+    Date.prototype.Format = function (fmt) { //author: meizz 
+        var o = {
+            "M+": this.getMonth() + 1, //月份 
+            "d+": this.getDate(), //日 
+            "h+": this.getHours(), //小时 
+            "m+": this.getMinutes(), //分 
+            "s+": this.getSeconds(), //秒 
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+            "S": this.getMilliseconds() //毫秒 
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+    revis = new Date().Format("yyyy-MM-dd");
+    //revis=date.toLocaleDateString();
     this.revision !== "" && this.revision !== undefined ?  revis = PRE + "\trevision " + this.revision + "{}\r\n":revis =  PRE + "\trevision " + revis + "{}\r\n" ;
     var descrp;
     if (typeof this.description == 'string') {
