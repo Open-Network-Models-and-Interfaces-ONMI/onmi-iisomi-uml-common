ee286788e35f8e4a2eafc009ac263a5f5aefdbff
demx8as6 <martin@skorupski.de>
2017-02-01 11:26:43 +0100

    give feature a short name without spaces

--- xmi2yang tool-v2.0/main.js
+++ xmi2yang tool-v2.0/main.js
@@ -1530,10 +1530,6 @@ function obj2yang(ele){
             if(ele[i].generalization.length > 0){
                 for(var j = 0; j < ele[i].generalization.length; j++) {
                     for (var k = 0; k < Typedef.length; k++) {
-                        if (!ele[i].attribute || !ele[i].attribute[0]) {
-                            var newEnum = new Type("enumeration");
-                            ele[i].attribute = [newEnum];
-                        }
                         if(ele[i].generalization[j] == Typedef[k].id){
                             ele[i].attribute[0].children = Typedef[k].attribute[0].children.concat(ele[i].attribute[0].children);
                             break;
@@ -2038,7 +2034,8 @@ function obj2yang(ele){
 
 //var m = 1;
 function createFeature(obj, path){
-    var feat = new Feature(obj.id, obj.condition, path, "",obj.fileName);
+    // TODO How to generate/create/define a propper feature name?
+    var feat = new Feature(obj.id, obj.id, path, obj.condition, obj.fileName);
     return feat;
 }
 
