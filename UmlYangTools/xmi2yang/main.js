6d5b857926ab12c788f0ba4753a5196bafdb73f6
Donald Hunter <donaldh@cisco.com>
2017-03-01 17:06:27 +0000

    type cannot contain units
    https://tools.ietf.org/html/rfc6020#section-7.4

--- UmlYangTools/xmi2yang/main.js
+++ UmlYangTools/xmi2yang/main.js
@@ -1839,17 +1839,14 @@ function obj2yang(ele){
                     if(k === Class.length){
                         console.warn("Warning: Cannot find type " + name + " for " + ele[i].name + '.' + ele[i].attribute[j].name + " - defaulting to string");
                         ele[i].attribute[j].nodeType === "list" ? ele[i].attribute[j].nodeType = "leaf-list" : ele[i].attribute[j].nodeType = "leaf";
->>>>>>> Warn about missing type information.
                         ele[i].attribute[j].type = "string";
                     }
                 }
                 if(ele[i].attribute[j].type.split("+")[0] === "leafref"){
-                    ele[i].attribute[j].type = new Type("leafref", ele[i].attribute[j].id, ele[i].attribute[j].type.split("+")[1], vr, "", "", units, ele[i].fileName);
+                    ele[i].attribute[j].type = new Type("leafref", ele[i].attribute[j].id, ele[i].attribute[j].type.split("+")[1], vr, "", "", ele[i].fileName);
                 }else if(ele[i].attribute[j].nodeType === "leaf" || ele[i].attribute[j].nodeType === "leaf-list"){
-                    ele[i].attribute[j].type = new Type(ele[i].attribute[j].type, ele[i].attribute[j].id, undefined, vr, "", "", units, ele[i].fileName);
-                }/*else{
-                 ele[i].attribute[j].type = new Type(ele[i].attribute[j].type, ele[i].attribute[j].id, undefined, vr, "", "", units, ele[i].fileName);
-                }*/
+                    ele[i].attribute[j].type = new Type(ele[i].attribute[j].type, ele[i].attribute[j].id, undefined, vr, "", "", ele[i].fileName);
+                }
                 if(ele[i].attribute[j].type.range !== undefined){
                     var regex  = /[^0-9/./*]/;
                     if(regex.test(ele[i].attribute[j].type.range) === true){
